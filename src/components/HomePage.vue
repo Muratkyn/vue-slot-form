e0502121416f3a867cb433c4824e05894d80d341
muratkoyun <mkoyun@red.software.systems>
2024-09-25 15:39:58 +0200

    'initial commit'

---
+++ src/components/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Enter your Cat informations</h2>
+    <Container> </Container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Container from "./shared/Container.vue";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/shared/Container.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({});
+</script>
+
+<template>
+  <div>
+    <slot name="Content"></slot>
+    <button>Prev</button>
+    <button>Next</button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
